b4a8ebf6fcf4bb9fd65c870050390bd9e6c20f7d
Ram Paranjothy <ram.paranjothy@amd.com>
2019-12-02 11:07:30 -0600

    styling..

--- sketch.js
+++ sketch.js
@@ -1,188 +1,103 @@
-// console.log("hi")
-
-// const setup = ()=>{
-//     console.log("hi")
-// }
-
-
-// const draw = ()=>{
-//     console.log("hi")
-// }
-
-
-
-
-// const movable=(e)=>{ return {
-//     show:(s)=>ellipse(e.l.x,e.l.y,s,s),
-//     drive:()=>e.l.add(e.a)
-
-// }
-// }
-
-
 class Vector {
-    constructor(x,y){
-        this.x=x
-        this.y=y
-    }
-    add(x){
-        this.x+=x.x;
-        this.y+=x.y
-    }
-    mul(x){
-        this.x*=x.x;
-        this.y*=x.y
-    }
-    print(){
-        console.log(this.x,this.y)
-    }
-    unpack(){
-        return this.x,this.y
-    }
-
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  add(x) {
+    this.x += x.x;
+    this.y += x.y;
+  }
+  mul(x) {
+    this.x *= x.x;
+    this.y *= x.y;
+  }
+  print() {
+    console.log(this.x, this.y);
+  }
+  unpack() {
+    return this.x, this.y;
+  }
 }
 
-class Engine{
-    constructor(x,y,xv,xa,yv,ya){
-        this.l=new Vector(x,y)
-        this.v=new Vector(xv,yv)
-        this.a=new Vector(xa,ya)
-        this.accelerate()
-
+class Engine {
+  constructor(x, y, xv, xa, yv, ya) {
+    this.l = new Vector(x, y);
+    this.v = new Vector(xv, yv);
+    this.a = new Vector(xa, ya);
+    this.accelerate();
+  }
+  accelerate() {
+    this.v = new Vector(random(-4, 4), random(-2, 2));
+    this.l.add(this.v);
+    if (this.l.x >= width) {
+      this.l.x = 0;
     }
-    accelerate(){
-        // if (this.v.x>4){
-        //     console.log("reducing")
-        //     this.a.x-=random(1)
-        // } 
-
-        // if (this.v.y>4){
-        //     console.log("reducing")
-        //     this.a.y-=random(1)
-        // } 
-      
-        // this.v.add(this.a)
-        this.v=new Vector(random(-4,4),random(-2,2))
-        this.l.add(this.v)
-        if (this.l.x>=width) {
-            // l=true
-            this.l.x=0
-        }
-        if (this.l.x<0) {
-            // l=true
-            this.l.x=width-0
-        }
-        if (this.l.y>=height) {
-            // l=true
-            this.l.y=0
-        }
-        if (this.l.y<0) {
-            // l=true
-            this.l.y=height-0
-        }
-        
-        this.draw()
+    if (this.l.x < 0) {
+      this.l.x = width - 0;
+    }
+    if (this.l.y >= height) {
+      this.l.y = 0;
+    }
+    if (this.l.y < 0) {
+      this.l.y = height - 0;
     }
 
-   
-    
+    this.draw();
+  }
 
-    draw(){
-        noStroke()
-        fill(random(180),random(180),random(150))
-        ellipse(this.l.x,this.l.y,10,10)
-    }
-
-    
-    
+  draw() {
+    noStroke();
+    fill(random(180), random(180), random(150));
+    ellipse(this.l.x, this.l.y, 5, 5);
+  }
 }
 
-const objs=[]
+const objs = [];
 
 function setup() {
-    x=0
-    createCanvas(600,400);
-    background(240)
-    l=new Vector(0,height/2)
-    v=new Vector(0,0)
-    a=new Vector(.01,0)
-    console.table(l,v,a)
-    // v1=new Engine(50,40,0,0.001,0,0)
-
-    
-  
-
+  x = 0;
+  createCanvas(windowWidth - 50, windowHeight - 20);
 }
 
+const addCircle = () => {
+  fill(random(255), random(255), random(255), random(255));
+  ellipse(random(0, width), random(0, height), random(100), random(45, 50));
+};
 
-
-const addCircle=()=>{
-    fill(random(255),random(255),random(255),random(255))
-    ellipse(random(0,width),random(0,height) , random(100), random(45,50));
+function mousePressed() {
+  objs.push(
+    new Engine(mouseX, mouseY, 0, random(-1, 1) / 100, 0, random(-1, 1) / 100)
+  );
 }
 
-
-
-function mousePressed(){
-    objs.push(new Engine(mouseX,mouseY,0,random(-1,1)/100,0,random(-1,1)/100))
-
-//    console.log(objs)
+function mouseMoved() {
+  mousePressed();
 }
 
+function draw() {
+  background(0);
 
-function mouseMoved(){
-    mousePressed()
-}
+  for (n of objs) {
+    n.accelerate();
+  }
+  // v1.accelerate()
+  // v1.draw()
 
-var draw=()=> {
+  // v2.accelerate()
+  // v2.draw()
 
-    background(20)
-    v.add(a)
-    l.add(v)
-    if (l.x>=width) {
-        l.x=0
-    }
-    
-    // ellipse(l.x,l.y,20,20)
+  //   ellipse(v1.l.x,v1.l.y,10,10)
 
-    
+  // v2.draw()
+  // v2.accelerate()
 
-    // for (n of nn){
-    //     objs.push(new Engine(random(0,width),random(0,height),0,random(-1,1)/100,0,0))
-    // }
-    // console.log(objs.length)
-    
-    for (n of objs){
-        n.accelerate()
-    }
-    // v1.accelerate()
-    // v1.draw()
+  //     // stroke()
+  //     stroke(255);
 
-    // v2.accelerate()
-    // v2.draw()
-    
-    
-    //   ellipse(v1.l.x,v1.l.y,10,10)        
+  //    line(0,0,random(width),random(height))
+  //    stroke(255,180,0,80);
 
-    // v2.draw()
-    // v2.accelerate()
-
-  
-    
-
-    
-   
-    
-
-
-    
-//     // stroke()
-//     stroke(255);
-
-//    line(0,0,random(width),random(height))
-//    stroke(255,180,0,80);
-   
-//    line(random(width),random(height),width,height)
-//    rect(random(x),random(height),10,random(height))
-//     x+=10
+  //    line(random(width),random(height),width,height)
+  //    rect(random(x),random(height),10,random(height))
+  //     x+=10
 }
